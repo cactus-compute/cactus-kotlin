f88bbc718ae769700c5324bfac088ebcc477085a
Abhi Jain <devabhixda@gmail.com>
2025-10-30 14:50:02 -0700

    Changes to model fetching and caching

--- library/src/commonMain/kotlin/services/ModelCache.kt
+++ library/src/commonMain/kotlin/services/ModelCache.kt
@@ -4,8 +4,8 @@ import com.cactus.CactusModel
 import com.cactus.VoiceModel
 
 internal expect object ModelCache {
-    suspend fun saveModels(models: List<CactusModel>)
-    suspend fun loadModels(): List<CactusModel>
+    suspend fun saveModel(model: CactusModel)
+    suspend fun loadModel(slug: String): CactusModel?
     suspend fun saveVoiceModels(models: List<VoiceModel>)
     suspend fun loadVoiceModels(): List<VoiceModel>
 }
